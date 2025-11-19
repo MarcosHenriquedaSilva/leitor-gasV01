356b395b1445e4c859d7f8996747b7ca3a2017de
devmhs <mhs.02@hotmail.com>
2025-11-19 13:22:47 -0300

    fix: add User type for Vercel build

---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  name?: string;
+  email?: string;
+  companyName?: string;
+  plan?: string;
+  maxUnits?: number;
+  password?: string;
+  createdAt?: string;
+}
